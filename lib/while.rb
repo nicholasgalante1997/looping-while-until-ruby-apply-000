ca6b792c568cf1be3b97455f5ab1092d929a9114
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-16 12:40:09 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,12 +1,6 @@
 def using_while
   levitation_force = 6
-	
-  while levitation_force <= 10 
-  puts "Wingardium Leviosa"
-  if levitation_force > 10 
-    break
-
-  
-end
+while levitation_force <= 10 
+puts "Wingardium Leviosa"
 
 
